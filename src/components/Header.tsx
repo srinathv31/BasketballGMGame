51fe13f9478dbabc026d2a39df909da188e14d1d
Srinath Venkatesh <srinathv@udel.edu>
2022-06-27 16:52:23 -0400

    header updates based on page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
+import { useAppSelector } from "../redux/hooks";
 
 export default function Header(): JSX.Element {
+    const index = useAppSelector(state => state.indexTracker.value);
+
+    const headerTexts = ["PHI", "PHI", "GAME 1 - 2022", "LEAGUE"];
+
     return(
         <View style={{ flex: 1, flexDirection: "row", alignItems: "flex-end", justifyContent: "center" }}>
             <Icon 
@@ -13,7 +18,7 @@ export default function Header(): JSX.Element {
                 style={{ position: "absolute", left: 5, bottom: 5 }}
                 onPress={() => console.log("CHEESE")}
             />
-            <Text style={{ padding: 10, fontWeight: "800", color: "#FBFAF5",  }}>PHI</Text>
+            <Text style={{ padding: 10, fontWeight: "800", color: "#FBFAF5" }}>{headerTexts[index]}</Text>
         </View>
     );
 }
