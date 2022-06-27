71449d2ef2f3ffdc7fe93cb4ca571e5d5d2f25f6
Srinath Venkatesh <srinathv@udel.edu>
2022-06-27 16:43:48 -0400

    bottom menu touch opacity

--- src/components/BottomMenuTab.tsx
+++ src/components/BottomMenuTab.tsx
@@ -1,6 +1,6 @@
 // Source Imports
 import React from "react";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { setByValue } from "../redux/indexSlice";
@@ -15,13 +15,13 @@ export default function BottomMenuTab(): JSX.Element {
         <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
             {menuButtons.map((item, idx) => {
                 return(
-                    <Icon
-                        key={idx}
-                        onPress={() => dispatch(setByValue(idx))}
-                        name={index === idx ? item : `${item}-outline`}
-                        size={25} 
-                        color="#900"
-                    ></Icon>
+                    <TouchableOpacity key={idx} onPress={() => dispatch(setByValue(idx))}>
+                        <Icon
+                            name={index === idx ? item : `${item}-outline`}
+                            size={25} 
+                            color="#900"
+                        ></Icon>
+                    </TouchableOpacity>
                 );
             })}
         </View>
