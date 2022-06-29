bed310d5f2ae9940f5f200f0f373ff545006ee70
Srinath Venkatesh <srinathv@udel.edu>
2022-06-29 18:42:43 -0400

    module declaration

--- src/screens/Overview.tsx
+++ src/screens/Overview.tsx
@@ -1,26 +1,11 @@
 // Source Imports
 import React from "react";
 import { View, Text } from "react-native";
-import { Image, SvgUri } from "react-native-svg";
 
 export default function Overview(): JSX.Element {
     return(
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            {/* <Image source={require("../assets/characterFeatures/face.svg")} style={{ width: 200, height: 200 }}></Image> */}
-            <Image
-                // x="5%"
-                // y="5%"
-                width="50%"
-                height="90%"
-                preserveAspectRatio="xMidYMid slice"
-                opacity="1"
-                href={require("../assets/characterFeatures/face.svg")}
-                clipPath="url(#clip)"
-            />
-            <SvgUri
-                width="25"
-                height="25"
-                uri={require("../assets/characterFeatures/face.svg")} />
+            <Text>OVERVIEW</Text>
         </View>
     );
 }
